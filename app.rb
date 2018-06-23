13853bb7897bd5b66b4ab874082933ae4931048c
Alex Zhang <alexgzhang92@gmail.com>
2018-06-23 20:05:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
   end
   
   post '/student' do
+    @student = Student.new(params[:student])
     
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
   end
 end
